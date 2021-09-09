cf7e39b267b357c5f519a3dbd49814ea5229a4e8
Guilherme Sanches <guilherme.sanchesVS@hotmail.com>
2021-09-09 13:42:04 -0300

    Application Web - Config Auth.

---
+++ src/web/NSE.WebApp.MVC/Configuration/AuthConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class AuthConfig
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/access-denied";
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentityController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentityController : Controller
+    {
+    }
+}
