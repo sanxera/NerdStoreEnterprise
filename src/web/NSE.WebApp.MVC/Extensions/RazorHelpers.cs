2fcfbb6d6cb0993b706c2764d7604987464506f4
Guilherme Sanches <guilherme.sanchesVS@hotmail.com>
2021-09-10 15:12:56 -0300

    Application Web - Alterações no layout da app.

---
+++ src/web/NSE.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public static class RazorHelpers
+    {
+        public static string HashEmailForGravatar(this RazorPage page, string email)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+            var sBuilder = new StringBuilder();
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        public static string FormatoMoeda(this RazorPage page, decimal value)
+        {
+            return value > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", value) : "Gratuito";
+        }
+
+        public static string MessageInventory(this RazorPage page, int amount)
+        {
+            return amount > 0 ? $"Apenas {amount} em estoque!" : "Produto esgotado!";
+        }
+    }
+}
--- src/web/NSE.WebApp.MVC/Models/UserViewModel/UserLogin.cs
+++ src/web/NSE.WebApp.MVC/Models/UserViewModel/UserLogin.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace NSE.WebApp.MVC.Models.UserViewModel
 {
     public class UserLogin
     {
+        [DisplayName("E-mail")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; }
 
+        [DisplayName("Senha")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; }
