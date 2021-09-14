2138c14700c188af74dc70eef6bc18245344656d
Guilherme Sanches <guilherme.sanchesVS@hotmail.com>
2021-09-14 17:44:50 -0300

    Client API - Validação de comandos.

--- src/services/NSE.Client.API/Application/Commands/RegisterClientCommand.cs
+++ src/services/NSE.Client.API/Application/Commands/RegisterClientCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentValidation;
 using NSE.Core.Messages;
 
 namespace NSE.Client.API.Application.Commands
@@ -16,6 +17,44 @@ namespace NSE.Client.API.Application.Commands
             Name = name;
             Email = email;
             Cpf = cpf;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RegisterClientValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class RegisterClientValidation : AbstractValidator<RegisterClientCommand>
+    {
+        public RegisterClientValidation()
+        {
+            RuleFor(c => c.Id)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage("O nome do cliente não foi informado");
+
+            RuleFor(c => c.Cpf)
+                .Must(CpfIsValid)
+                .WithMessage("O CPF informado não é válido.");
+
+            RuleFor(c => c.Email)
+                .Must(EmailIsValid)
+                .WithMessage("O e-mail informado não é válido.");
+        }
+
+        protected static bool CpfIsValid(string cpf)
+        {
+            return Core.DomainObjects.Cpf.Validar(cpf);
+        }
+
+        protected static bool EmailIsValid(string email)
+        {
+            return Core.DomainObjects.Email.Validate(email);
         }
     }
 }
