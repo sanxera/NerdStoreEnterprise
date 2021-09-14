1d95b10b44e921adae652e21d4bc6516f5c11fe6
Guilherme Sanches <guilherme.sanchesVS@hotmail.com>
2021-09-14 13:54:58 -0300

    Client API - Objetos de valor, entidades e validações.

---
+++ src/services/NSE.Client.API/Models/Address.cs
@@ -0,0 +1,25 @@
+﻿namespace NSE.Client.API.Models
+{
+    public class Address
+    {
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string District { get; private set; }
+        public string ZipCod { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public Client Client { get;  protected set; }
+
+        public Address(string street, string number, string complement, string district, string zipCod, string city, string state)
+        {
+            Street = street;
+            Number = number;
+            Complement = complement;
+            District = district;
+            ZipCod = zipCod;
+            City = city;
+            State = state;
+        }
+    }
+}
---
+++ src/services/NSE.Client.API/Models/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Client.API.Models
+{
+    public class Client : Entity
+    {
+        public string Name { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
+        public bool Deleted { get; private set; }
+        public Address Address { get; private set; }
+
+        protected Client() { }
+        public Client(Guid id, string name, string email, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
+            Deleted = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Address address)
+        {
+            Address = address;
+        }
+    }
+}
